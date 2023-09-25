90d20d1471c3a996ddbf0f4bef79996bbd5dd27f
rguido <rodrigoguidoarias92@gmail.com>
2023-09-25 10:31:45 +0200

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class App extends JFrame {
+    JPanel panel;
+    JLabel label;
+    JTextField textField;
+    JCheckBox checkBox;
+    GridBagConstraints c;
+    //String cad = "";
+
+    App() {
+
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+
+        c.insets = new Insets(20, 20, 20, 20);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        label = new JLabel("Introduce un texto cualquiera");
+        // centra la label horizontalmente
+        label.setHorizontalAlignment(0);
+        panel.add(label, c);
+
+        textField = new JTextField();
+        // c.insets=new Insets(20, 20, 20, 20);
+        c.weightx = 0.5;
+        c.weighty = 0.0;
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(textField, c);
+
+        checkBox = new JCheckBox("Convertir a mayusculas");
+        c.gridy = 2;
+        checkBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (checkBox.isSelected()) {
+                    textField.setText(textField.getText().toUpperCase());
+                }else textField.setText(textField.getText().toLowerCase(getLocale()));
+                
+            }
+        });
+        panel.add(checkBox, c);
+        setContentPane(panel);
+        setResizable(false);
+        setSize(600, 200);
+        // centra la ventana
+        setLocationRelativeTo(null);
+        setTitle("Ejemplo de uso de JCheckBox");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        App a = new App();
+
+    }
+}
